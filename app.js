4607db4d9a9bf1bc7831501a9623d7b03cae8720
Gabriel Rosa <grosa.ict@gmail.com>
2020-09-13 18:52:45 -0300

    Até exercício 3 aula 2

--- app.js
+++ app.js
@@ -2,27 +2,35 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.get('/product', (req, res) => {
+    res.json([{id:1, name:"produto 1", price:30.000}])
     res.send('Rota para buscar todos os produtos')
 })
    
 app.get('/product/:id', (req, res) => {
+    res.json([{id:1, name:"produto 1", price:30.000}])
     res.send('Rota para buscar o produto '+ req.params.id)
 })
    
 app.post('/product', (req, res) => {
+    res.json(req.body)
     res.send('Rota para incluir um produto')
 })
    
 app.put('/product/:id', (req, res) => {
+    res.json([{id:1, name:"produto 1", price:30.000}])
     res.send('Rota para alterar o produto '+ req.params.id)
 })
    
 app.delete('/product/:id', (req, res) => {
+    res.json([{id:1, name:"produto 1", price:30.000}])
     res.send('Rota para apagar o produto '+ req.params.id)
 })
 

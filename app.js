aa8a5f70a5aca3e93117d939afc6ba1186bebc83
Gabriel Rosa <grosa.ict@gmail.com>
2020-09-14 20:20:11 -0300

    Rotas no exercício 4 e início exercício 6

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const controller = require('./controllers/controller')
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express') //import express
+const app = express() //creates const for use express
+const port = 3000 //define application port
+//import routes of product
+const productRoute = require('./routes/product_routes')
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+/* const controller = require('./controllers/product_controller')
 app.get     ('/product',        controller.list)
 app.get     ('/product/:id',    controller.getById)
 app.post    ('/product',        controller.insert)
 app.put     ('/product/:id',    controller.update)
-app.delete  ('/product/:id',    controller.delete)
+app.delete  ('/product/:id',    controller.delete) */ 
+
+//creates a route base for product
+app.use('/api/product', productRoute)
 
 app.listen(port, () => {
     console.log(`Iniciando o servidor: http://localhost:${port}`)
---
+++ routes/product_routes.js
@@ -0,0 +1,14 @@
+const express = require('express') //import express
+
+const router = express.Router() //creates const for use function Router()
+module.exports = router //export router for use in other .js
+
+//creates const for use product controller
+const controller = require('../controllers/product_controller')
+
+router.get      ('/',       controller.list)
+router.get      ('/:id',    controller.getById)
+router.post     ('/',       controller.insert)
+router.put      ('/:id',    controller.update)
+router.delete   ('/:id',    controller.delete)
+

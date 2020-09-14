01025754d1f1f5a026a85f80be8f874ab1e9a824
Gabriel Rosa <grosa.ict@gmail.com>
2020-09-14 09:04:16 -0300

    Complemento exercício 4 aula 2

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const port = 3000
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.get('/product',         controller.list)
-app.get('/product/:id',     controller.getById)
-app.post('/product',        controller.insert)
-app.put('/product/:id',     controller.update)
-app.delete('/product/:id',  controller.delete)
+app.get     ('/product',        controller.list)
+app.get     ('/product/:id',    controller.getById)
+app.post    ('/product',        controller.insert)
+app.put     ('/product/:id',    controller.update)
+app.delete  ('/product/:id',    controller.delete)
 
 app.listen(port, () => {
     console.log(`Iniciando o servidor: http://localhost:${port}`)
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,34 +1,67 @@
 exports.list = (req, res) => {
-    obj = [
+    let products = new Object();
+    products = [
         {id:1, name:"Produto A", price:30000},
         {id:2, name:"Produto B", price:25000},
         {id:3, name:"Produto C", price:30600}
     ]
-    res.json(obj)
+    res.json(products)
 }
 
 exports.getById = (req, res) => {  
-    res.json({id:req.params.id, name:"Produto X", price:22000})
+    //res.json({id:req.params.id, name:"Produto X", price:22000})
+    if(req.params.id == 1){
+        let product = new Object();
+        product.id = req.params.id;
+        product.name = 'Produto X';
+        product.price = 22000;
+        res.json(product);
+    }
+    else {
+        res.sendStatus(404);
+    }
 }
    
 exports.insert = (req, res) => {
-    res.json({id:1, name:req.body.name, price:req.body.price})
-    /*
-        To send simple request use: 
+    /*  To send simple request use: 
             res.json(req.body)
 
         Object example to use in Postman:
         {
             "name":     "Produto A",
-            "price":    "1000"
+            "price":    1000
         }
-    */
+    res.json({id:1, name:req.body.name, price:req.body.price})*/
+    let product = new Object();
+        product.id = 1;
+        product.name = req.body.name;
+        product.price = req.body.price;
+    res.status(201).json(product);
 }
    
 exports.update = (req, res) => {
-    res.json({id:req.params.id, name:"Produto X", price:22000})
+    //res.json({id:req.params.id, name:"Produto X", price:22000})
+    if(req.params.id == 2){
+        let product = req.body;
+        product.id = req.params.id;
+        product.price += 22000;
+        res.json(product);
+    }
+    else {
+        res.sendStatus(404);
+    }
 }
    
 exports.delete = (req, res) => {
-    res.json({id:req.params.id, name:"Produto X", price:22000})
+    //res.json({id:req.params.id, name:"Produto X", price:22000})
+    if(req.params.id == 3){
+        let product = new Object();
+        product.id = req.params.id;
+        product.name = 'Produto X';
+        product.price = 22000;
+        res.json(product);
+    }
+    else {
+        res.sendStatus(404);
+    }
 }
